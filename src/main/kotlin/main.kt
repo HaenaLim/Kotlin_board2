3e5a4b2f77f7bd23015b30ccd4118e923ae5140c
HaenaLim <dlagoskekd@naver.com>
2021-04-14 23:14:02 +0900

    명령어

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    println("== 게시판 관리 프로그램")
+
+    while (true){
+        print("명령어")
+        val command = readLine() //val 바꿀가능성 없다 var 바꿀가는성있다
+        println("입력한 명령어 :$command") //$변수명-변수다..
+//git checkout -f .  (마지막커밋으로 돌아가라)
+
+    }
+
+    println("==게시판 관리 프로그램끝끝 ==")
+}
